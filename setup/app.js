1981dd3988a598684f36f1727fb948cdd7cdb611
Dan Possehl <danielbpossehl@gmail.com>
2021-12-02 14:27:02 -0700

    added toggle instead of if else statment

--- setup/app.js
+++ setup/app.js
@@ -9,4 +9,5 @@ toggleBtn.addEventListener("click", function () {
   // if (sidebar.classList.contains("show-sidebar")) {
   //   sidebar.classList.remove("show-sidebar");
   // } else sidebar.classList.add("show-sidebar");
+  sidebar.classList.toggle("show-sidebar");
 });
